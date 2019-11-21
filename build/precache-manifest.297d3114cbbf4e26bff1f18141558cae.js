9de8c9a5f1d076395d426426676392ab876b4efc
Alex <43651369+tunkytime@users.noreply.github.com>
2019-11-21 13:34:15 -0700

    Removed react boilerplate

---
+++ build/precache-manifest.297d3114cbbf4e26bff1f18141558cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d21d4b11aa2ffb96d7e8e4f655a8959",
+    "url": "/code-screen/index.html"
+  },
+  {
+    "revision": "9dc11512779d358f5426",
+    "url": "/code-screen/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "f16b94b6ef5b11c4c0bc",
+    "url": "/code-screen/static/js/2.099a891a.chunk.js"
+  },
+  {
+    "revision": "9dc11512779d358f5426",
+    "url": "/code-screen/static/js/main.b96b5c1f.chunk.js"
+  },
+  {
+    "revision": "23aa19b99fcc323f00bb",
+    "url": "/code-screen/static/js/runtime-main.9c728180.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/code-screen/static/media/logo.25bf045c.svg"
+  }
+]);
